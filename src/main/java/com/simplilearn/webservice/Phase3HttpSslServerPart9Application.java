3c8592e9b73909c5cef54e1b4b0a8b6856701fd3
Anushka Sharma <anushkasharma2109@gmail.com>
2021-10-19 19:14:48 +0530

    added http ssl server

---
+++ src/main/java/com/simplilearn/webservice/Phase3HttpSslServerPart9Application.java
@@ -0,0 +1,13 @@
+package com.simplilearn.webservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Phase3HttpSslServerPart9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Phase3HttpSslServerPart9Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/simplilearn/webservice/Phase3HttpSslServerPart9ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simplilearn.webservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Phase3HttpSslServerPart9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
